d625b2c778a9dedfb9bdaa3db87f268d821e4717
jbergay <jbergay@jgingenieros.es>
2016-10-25 12:50:19 +0200

    feat(createAppTitleStore): created app title store tests: added tests

--- src/store.spec.ts
+++ src/store.spec.ts
@@ -1,18 +1,119 @@
-
 "use strict";
 
-import 'jest';
+import "jest";
 require("babel-core/register");
 require("babel-polyfill");
+import { Observable } from "rxjs/Observable";
+import { queue } from "rxjs/scheduler/queue";
+import "rxjs/add/observable/concat";
+import "rxjs/add/observable/empty";
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/concat";
+import "rxjs/add/operator/delay";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/first";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/last";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/observeOn";
+import "rxjs/add/operator/subscribeOn";
+import "rxjs/add/operator/switchMap";
+import "rxjs/add/operator/takeLast";
+import "rxjs/add/operator/timeout";
+import "rxjs/add/operator/toPromise";
+import * as deepEqual from "deep-equal";
+import {
+    reassign, Store, Action, StoreActions, logUpdates, startEffects,
+    tunnelActions, ActionTunnel,
+} from "rxstore";
+import { testActions, expectedActions } from "rxstore-jest";
+import {
+    testUpdateEffects, testActionEffects, testStateEffects,
+    expectAction, expectItem, testLastStateEffects,
+} from "rxstore-jest";
 
-import { Example } from "./store";
+import { defaultAppTitleState, createAppTitleStore, AppTitleState, AppTitleStore, AppTitleActions } from "./index";
 
-describe("Example", () => {
-    it("Should be pass sanity", () => {
-        expect(typeof Example).toBe("function");
+describe("defaultAppTitleState", () => {
+    describe("Sanity checks", () => {
+        it("Should be a function", () => {
+            expect(typeof defaultAppTitleState).toBe("function");
+        });
     });
 
-    it("Should be able to create new instance", () => {
-        expect(typeof new Example()).toBe("object");
+    describe("Given no options", () => {
+        it("The default state should have default values", () => {
+            const state = defaultAppTitleState();
+            expect(state.title).toEqual("MantTest 2.0");
+            expect(typeof state.onMainButtonClick).toBe("function");
+            expect(state.mainButtonIcon).toBeUndefined;
+            expect(typeof state.messagesStore).toBe("object");
+            expect(typeof state.snackBarMessagesStore).toBe("object");
+        });
     });
+});
+
+describe("createAppTitleStore", () => {
+    describe("Sanity checks", () => {
+        it("should be a function", () => expect(typeof createAppTitleStore).toBe("function"));
+    });
+
+    testLastStateEffects<AppTitleState, AppTitleStore>("Given a defaultAppTitleState", createAppTitleStore())
+        ("When the store receives no actions", "The state should be as expected", [],
+        state => {
+            expect(state.mainButtonIcon).toEqual(defaultAppTitleState().mainButtonIcon);
+            expect(typeof state.messagesStore).toBe("object");
+            expect(typeof state.onMainButtonClick).toBe("function");
+            expect(typeof state.snackBarMessagesStore).toBe("object");
+            expect(state.title).toEqual(defaultAppTitleState().title);
+        });
+});
+
+const init: AppTitleState = defaultAppTitleState();
+const initSetup: AppTitleState = reassign(init, {
+    onMainButtonClick: console.log,
+    title: "title",
+    mainButtonIcon: "mainButtonIcon",
+});
+
+testActions(AppTitleActions, "AppTitleActions",
+    expectedActions<AppTitleState>("MantTest.AppTitle/",
+        actions => {
+            actions.typed("setAppBar", "SET_APP_BAR")
+                .withSample(
+                    init, {
+                        onMainButtonClick: console.log,
+                        title: "title",
+                        mainButtonIcon: "mainButtonIcon",
+                    }, initSetup);
+
+            actions.empty("toggleSettings", "TOGGLE_SETTINGS");
+
+            actions.typed("loadMessages", "LOAD_MESSAGES");
+
+            actions.empty("cleanMessages", "CLEAN_MESSAGES");
+
+            actions.empty("logOut", "LOG_OUT");
+}));
+
+describe("AppTitleEvents", () => {
+
+    describe("When the event cleanMessages is called", () => {
+      it("should dispatch a CLEAN_MESSAGES action", () => {
+          const store = createAppTitleStore()();
+          store.action$.subscribe(a => expect(a).toEqual(AppTitleActions.cleanMessages()));
+          store.cleanMessages();
+      });
+    });
+
+    describe("When the event logOut is called", () => {
+      it("should dispatch a LOG_OUT action", () => {
+          const store = createAppTitleStore()();
+          store.action$.subscribe(a => expect(a).toEqual(AppTitleActions.logOut()));
+          store.logOut();
+      });
+    });
+
+  
 });
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,87 @@
-export class Example {
-    private covered: boolean = false;
-    /* Some code in here :) */
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+import "rxjs/add/observable/merge";
+import "rxjs/add/operator/distinctUntilChanged";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/switchMap";
+import "rxjs/add/operator/startWith";
+import "rxjs/add/operator/observeOn";
+import "rxjs/add/operator/subscribeOn";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/timeout";
+import * as deepEqual from "deep-equal";
+import {
+  reassign, reassignif,
+  actionCreator, TypedActionDescription, EmptyActionDescription,
+  reducerFromActions, Reducer, StateUpdate,
+  createStore, Store, StoreMiddleware,
+  withEffects, defineStore, ICreateStoreOptions, logUpdates,
+  tunnelActions, extendWithActions, extendWith,
+} from "rxstore";
+import { MessagesStore, MessagesModel, createMessagesStore, alertEffects } from "manttest-client-messages-store";
 
-    public coverageTest() {
-        /* Should be uncovered. */
-        this.covered = true;
-    }
+/* MODELS */
+
+export interface AppTitleState {
+  title: string;
+  onMainButtonClick: () => void;
+  mainButtonIcon: any;
+  messagesStore: MessagesStore;
+  snackBarMessagesStore: MessagesStore;
 }
+
+export interface SetAppBarPayload {
+  title: string;
+  onMainButtonClick: () => void;
+  mainButtonIcon: any;
+}
+
+/* ACTIONS */
+
+export interface AppTitleEvents {
+  cleanMessages(): void;
+  logOut(): void;
+}
+
+const newEvent = actionCreator<AppTitleState>("MantTest.AppTitle/");
+
+export const AppTitleActions = {
+
+  setAppBar: newEvent.of<SetAppBarPayload>(
+    "SET_APP_BAR",
+    (s, p) => reassign(s, {
+      onMainButtonClick: p.onMainButtonClick,
+      title: p.title,
+      mainButtonIcon: p.mainButtonIcon,
+    })
+  ),
+
+  toggleSettings: newEvent("TOGGLE_SETTINGS"),
+
+  loadMessages: newEvent.of<MessagesModel[]>("LOAD_MESSAGES"),
+
+  cleanMessages: newEvent("CLEAN_MESSAGES"),
+
+  logOut: newEvent("LOG_OUT"),
+};
+
+/* STORE */
+
+const AppTitleReducer = reducerFromActions(AppTitleActions);
+
+export type AppTitleStore = Store<AppTitleState> & AppTitleEvents;
+
+export const defaultAppTitleState = (): AppTitleState => ({
+  title: "MantTest 2.0",
+  onMainButtonClick: () => undefined,
+  mainButtonIcon: undefined,
+  messagesStore: createMessagesStore()(),
+  snackBarMessagesStore: createMessagesStore()({ middlewaresAfter: [withEffects(alertEffects({ timeout: 0 }))] }),
+});
+
+export const createAppTitleStore = () => defineStore<AppTitleState, AppTitleStore>(
+  AppTitleReducer,
+  defaultAppTitleState,
+  extendWithActions(AppTitleActions)
+);
